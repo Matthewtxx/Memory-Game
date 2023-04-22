97bb5fff97da6283e8f5521083f648613fbd7b99
Matthew <Matthewtxx@hotmail.com>
2023-04-22 13:43:49 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const tilesContainer = document.querySelector(".tiles");
+const colors = ['lightpink', 'violet', 'crimson', 'blue', 'dodgerblue', 'yellow', 'orange', 'teal'];
+const colorsPickList = [...colors, ...colors];
+const tileCount = colorsPickList.length;
+
+//Game State
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
+
+function buildTile(color){
+    const element = document.createElement('div');
+
+    element.classList.add('tile');
+    element.setAttribute('data-color', color);
+    element.setAttribute('data-revealed', 'false');
+
+    element.addEventListener('click', () => {
+        const revealed = element.getAttribute('data-revealed');
+
+        if(awaitingEndOfMove || revealed === 'true' || element === activeTile) {
+            return;
+        }
+        
+        element.style.backgroundColor = color;
+
+        if(!activeTile) {
+            activeTile = element;
+
+            return;
+        }
+
+        const colorToMatch = activeTile.getAttribute('data-color');
+
+        if(colorToMatch === color) {
+
+            activeTile.setAttribute('data-revealed', 'true');
+            element.setAttribute('data-revealed', 'true');
+
+
+            activeTile = null;
+            awaitingEndOfMove = false;
+            revealedCount += 2;
+
+            if(revealedCount === tileCount) {
+                alert('you win');
+            }
+            return;
+        }
+
+        awaitingEndOfMove = true;
+        setTimeout(() => {
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+
+            awaitingEndOfMove = false;
+            activeTile = null;
+        }, 1000)
+    });
+    return element;
+}
+
+//Build up tiles
+for(let i = 0; i < tileCount; i++) {
+    const randomIndex = Math.floor(Math.random() * colorsPickList.length);
+    const color = colorsPickList[randomIndex];
+    const tile = buildTile(color);
+    
+    colorsPickList.splice(randomIndex, 1);
+    tilesContainer.appendChild(tile);
+}
+
